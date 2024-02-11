82b80787952d31b9d54b0ccd259bfb70cdf6705e
saiful797 <saifulislamsaif797@gmail.com>
2024-02-11 13:38:53 +0600

    added js file

--- JS/events.js
+++ JS/events.js
@@ -1,6 +1,37 @@
-console.log('This is separate JS file.');
+// Option 1: Directly set on the HTML elements
+    //<button onclick="console.log(7)">click Me</button>
 
-//Type 2: onclick events
+//option 2: add onclick function
+    //<button onclick="makeRed()">Make Red</button>
 function makeRed(){
     document.body.style.backgroundColor='red';
 }
+
+//option: 3
+
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+//option 3 another
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+     document.body.style.backgroundColor = 'purple';
+}
+
+// option 4
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+
+ function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option 4 another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+})
